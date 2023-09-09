f52ef827d71e2609d3317fac8711d6fc7a85e9b3
anjoo91 <andrew.w.lew@gmail.com>
2023-09-08 22:20:41 -0400

    updated AJAX to JS to force loading of urls when tab is clicked

--- main_app/views.py
+++ main_app/views.py
@@ -4,15 +4,9 @@ from django.template.loader import render_to_string
 
 # Create your views here.
 def home(request):
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        content = render_to_string('home_content_block.html')
-        return JsonResponse({'content': content})
     return render(request, 'home.html')
 
 def regex(request):
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        content = render_to_string('regex_content_block.html')
-        return JsonResponse({'content': content})
     return render(request, 'regex.html')
 
 def lemma(request):
