5dae6803e1b1fbdd53765e0e525e00e408d9e8ba
anjoo91 <andrew.w.lew@gmail.com>
2023-09-11 15:44:38 -0400

    added regex view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+import re
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def regex(request):
-    print("Inside regex view")  # Debugging line
-    return render(request, 'regex.html')
+    if request.method =="POST":
+        phonepattern=r'(\d{3})-(\d{3})-(\d{4})'
+        regex_area = request.POST.get('regex-form')
+        regex_phone=re.findall(phonepattern, regex_area)
+        return render(request, 'regex.html', {'regex_phone': regex_phone})
+    else:
+        return render(request, 'regex.html')
 
 def lemma(request):
     return render(request, 'lemma.html')
