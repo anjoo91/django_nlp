d87606bea287a8e276813f1a073568950219ca9d
anjoo91 <andrew.w.lew@gmail.com>
2023-09-12 03:26:39 -0400

    removed vercel files; deployment failed consistently

--- django_nlp/settings.py
+++ django_nlp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-))=)3d5&lhs9$1ninvh-s0dzmx^=73p-a0p&%ht6v#2jrpmhzm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
