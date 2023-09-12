d1cd8413e24dabe058d4f446ed67a1018dab4234
anjoo91 <andrew.w.lew@gmail.com>
2023-09-12 01:53:14 -0400

    updated hosts allowed in settings.py

--- django_nlp/settings.py
+++ django_nlp/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-))=)3d5&lhs9$1ninvh-s0dzmx^=73p-a0p&%ht6v#2jrpmhzm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
