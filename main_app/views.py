aaa3641e9ac0584bb28daad0f99c5e065f8e12d1
anjoo91 <andrew.w.lew@gmail.com>
2023-09-11 21:06:45 -0400

    added updates to views.py for lemma, pos, and ner

--- main_app/views.py
+++ main_app/views.py
@@ -19,32 +19,44 @@ def regex(request):
         return render(request, 'regex.html')
 
 def lemma(request):
+    nlp = spacy.load('en_core_web_sm')
     if request.method == 'POST':
-        text = request.POST.get('inputText')
-        nlp = spacy.load("en_core_web_sm")
-        doc = nlp(text)
-        lemmas = [token.lemma_ for token in doc]
-        return render(request, 'lemma.html', {'lemmas': lemmas})
+        lemma_area = request.POST['lemma-form']
+        lemma_area = nlp(lemma_area)
+        token_list = [i.text for i in lemma_area]
+        tokens=[i for i in token_list]
+        lemmas_list = [i.lemma_ for i in lemma_area]
+        lemmas=[i for i in lemmas_list]
+        return render(request, 'lemma.html', {'unprocessed': tokens, 'lemmatized': lemmas})
     else:
         return render(request, 'lemma.html')
 
 def pos(request):
     nlp = spacy.load('en_core_web_sm')
     if request.method == "POST":
-        pos_area = request.POST.get('pos-form')
-        doc = nlp(pos_area)
-        pos_list = [(token.text, token.pos_) for token in doc]
-        return render(request, 'pos.html', {'pos_list': pos_list})
+        pos_area = request.POST['pos-form']
+        pos_area = nlp(pos_area)
+        pos_tokens_list = [i.text for i in pos_area]
+        pos_tokens=[i for i in pos_tokens_list]
+        pos_results_list=[i.pos_ for i in pos_area]    
+        pos_results = [i for i in pos_results_list] 
+        explain_pos_list=[spacy.explain(i) for i in pos_results]  
+        explain_individual_pos=[i for i in explain_pos_list]
+        return render(request, 'pos.html', {'unpossed': pos_tokens, 'possed': pos_results, 'explain_pos': explain_individual_pos})
     else:
         return render(request, 'pos.html')
 
 def ner(request):
     nlp = spacy.load('en_core_web_sm')
     if request.method == "POST":
-        ner_area = request.POST.get('ner-form')
-        doc = nlp(ner_area)
-        ner_list = [(ent.text, ent.label_) for ent in doc.ents]
-        return render(request, 'ner.html', {'ner_list': ner_list})
+        ner_area = request.POST['ner-form']
+        ner_area = nlp(ner_area)
+        ner_tokenslabels_list = [(i.text, i.label_) for i in ner_area.ents]
+        ner_tokenslabels = [i for i in ner_tokenslabels_list]
+        onlylabels = [i.label_ for i in ner_area.ents]
+        ner_explain_list = [spacy.explain(i) for i in onlylabels]
+        ner_explain_individual=[i for i in ner_explain_list]
+        return render(request, 'ner.html', {'nertokenslabels': ner_tokenslabels, 'ner_explain': ner_explain_individual})
     else:
         return render(request, 'ner.html')
 
